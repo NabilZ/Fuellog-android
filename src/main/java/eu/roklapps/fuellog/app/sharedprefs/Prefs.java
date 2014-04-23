a82d79e54cc463dbe60199974bd11db59de162a4
Roman <0elendil73@gmail.com>
2014-04-23 19:08:04 +0200

    Fluffy AsnycTask!

--- src/main/java/eu/roklapps/fuellog/app/sharedprefs/Prefs.java
+++ src/main/java/eu/roklapps/fuellog/app/sharedprefs/Prefs.java
@@ -1,13 +1,20 @@
 package eu.roklapps.fuellog.app.sharedprefs;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
 public class Prefs {
     public static final String PREFS_NAME = "fuelpref";
     public static final String KEY_SUBFRAGMENT_OPEN = "subfragment_car_active";
 
-    public static void setCarSubFragment(Context context, boolean value) {
-        context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE).edit().putBoolean(Prefs.KEY_SUBFRAGMENT_OPEN, value).apply();
+    public static void setCarSubFragment(final Context context, final boolean value) {
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... params) {
+                context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE).edit().putBoolean(Prefs.KEY_SUBFRAGMENT_OPEN, value).apply();
+                return null;
+            }
+        }.execute();
     }
 
     public static boolean isCarSubFragmentActive(Context context) {
